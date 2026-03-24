58f7e3426b2b7ea29214d5d5ae7710a83e7ffc2c
Ruan Yuping <ruanyuping@MacBook-Pro-52.local>
2026-03-24 15:14:26 +0800

    Fix build.gradle.kts: remove duplicate buildscript and allprojects blocks

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,3 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
